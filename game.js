336aa855e1c368cd02d1f5016b531afe83f5dc37
jbfly <john.bonewitz@gmail.com>
2023-03-30 23:27:45 -1000

    made poo autospawn, added grid back

--- game.js
+++ game.js
@@ -1,63 +1,54 @@
-// Import Phaser library
 import Phaser from 'phaser';
 
-// Define constants for tile size and map size
 const tileSize = 38;
 const mapSize = 50;
 
-// Create a new IsoScene class that extends Phaser.Scene
 class IsoScene extends Phaser.Scene {
-    // Define the constructor for the IsoScene class
     constructor() {
-        // Call the constructor of the superclass Phaser.Scene and set the scene key
         super({ key: 'IsoScene' });
     }
 
-    // Create game objects and set up the game state
-    create() {
-        // Create a new group for isometric game objects
-        this.isoGroup = this.add.group();
-
-        // Create the grid
-        this.grid = this.createGrid(mapSize, mapSize, tileSize);
-
-        // Initialize an array to store poo objects
-        this.poos = [];
-
-        // Add spacebar key event listener
-        this.spaceKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    preload() {
+        this.load.image('highlight', 'assets/highlight.png');
     }
 
-    // Update the game state, called every frame
-    update() {
-        // Check if the spacebar is pressed
-        if (this.spaceKey.isDown) {
-            // Call the createPoo method to create a poo object
+    create() {
+        this.isoGroup = this.add.group();
+        this.grid = this.createGrid(mapSize, mapSize, tileSize);
+
+        this.poos = [];
+        this.spawnRate = 1000;
+        this.lastSpawnTime = 0;
+
+        this.startPoint = {
+            x: Math.floor(mapSize / 2),
+            y: Math.floor(mapSize / 2)
+        };
+
+        this.input.on('pointermove', (pointer) => {
+            const { x, y } = this.getGridCoordinates(pointer.x, pointer.y);
+            if (x >= 0 && x < mapSize && y >= 0 && y < mapSize) {
+                this.highlightGrid(x, y);
+            }
+        });
+    }
+
+    update(time) {
+        if (time > this.lastSpawnTime + this.spawnRate) {
             this.createPoo();
+            this.lastSpawnTime = time;
         }
 
-        // Iterate through the poos array and update each poo object
         this.poos.forEach(poo => {
-            // Update poo position based on its speed
-            poo.x += poo.speed;
-            poo.y += poo.speed;
-            poo.height += poo.speed;
-
-            // Check if the poo has reached the edge of the map
-            if (poo.x >= 10 * 38) {
-                // Reset the poo position and height
-                poo.x = 0;
-                poo.y = 0;
-                poo.height = 0;
+            if (poo.height < tileSize * 5) {
+                poo.height += poo.speed;
             }
 
-            // Update the poo's tile position and depth based on its new position
-            poo.tile.setPosition(poo.x, poo.y);
+            poo.tile.setPosition(poo.x, poo.y - poo.height);
             poo.tile.depth = poo.y + poo.height;
         });
     }
 
-    // Create a grid system
     createGrid(rows, cols, size) {
         const grid = [];
         for (let y = 0; y < rows; y++) {
@@ -68,41 +59,55 @@ class IsoScene extends Phaser.Scene {
                 tile.setOrigin(0, 0);
                 grid[y][x] = {
                     tile: tile,
-                    object: null
+                    object: null,
+                    highlight: null
                 };
             }
         }
         return grid;
     }
 
-    // Create a poo object and add it to the poos array
     createPoo() {
-        const randomX = Math.floor(Math.random() * mapSize);
-        const randomY = Math.floor(Math.random() * mapSize);
+        const x = this.startPoint.x;
+        const y = this.startPoint.y;
 
-        const pooTile = this.add.text(randomX * tileSize, randomY * tileSize, '💩', { fontSize: '32px' });
+        const pooTile = this.add.text(x * tileSize, y * tileSize, '💩', { fontSize: '32px' });
         pooTile.setOrigin(0, 0);
         this.isoGroup.add(pooTile);
 
         const poo = {
-            x: randomX * tileSize,
-            y: randomY * tileSize,
+            x: x * tileSize,
+            y: y * tileSize,
             height: 0,
-            speed: 2,
+            speed: 0.5,
             tile: pooTile
         };
 
         this.poos.push(poo);
     }
+
+    highlightGrid(x, y) {
+        if (this.grid[y][x].highlight) {
+            this.grid[y][x].highlight.destroy();
+        }
+
+        const highlight = this.add.image(x * tileSize, y * tileSize, 'highlight');
+        highlight.setOrigin(0, 0);
+        this.grid[y][x].highlight = highlight;
+    }
+
+    getGridCoordinates(x, y) {
+        const gridX = Math.floor(x / tileSize);
+        const gridY = Math.floor(y / tileSize);
+        return { x: gridX, y: gridY };
+    }
 }
 
-// Define the configuration object for the Phaser game
 const config = {
     type: Phaser.AUTO,
     width: mapSize * tileSize,
     height: mapSize * tileSize,
     scene: IsoScene,
-};
-
-// Create a new Phaser game instance with the configuration object
-const game = new Phaser.Game(config);
+    };
+    
+    const game = new Phaser.Game(config);
