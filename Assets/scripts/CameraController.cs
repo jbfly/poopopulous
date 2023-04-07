844539c1d6a58677ddad4e4a86905e85f08bdf86
jbfly <john.bonewitz@gmail.com>
2023-04-07 13:00:11 -1000

    Panning works!

--- Assets/scripts/CameraController.cs
+++ Assets/scripts/CameraController.cs
@@ -23,11 +23,11 @@ public class CameraController : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
-        // Negate the vertical input to reverse up/down panning
-        Vector3 panMovement = new Vector3(horizontal, 0, vertical) * panSpeed * Time.deltaTime;
+        // Calculate the pan movement based on the camera's local right and local forward vectors
+        Vector3 panMovement = (cam.transform.right * horizontal + cam.transform.forward * vertical) * panSpeed * Time.deltaTime;
 
-        // Rotate panMovement by 135 degrees around the Y-axis
-        panMovement = Quaternion.Euler(0, 135, 0) * panMovement;
+        // Remove any vertical movement component from the pan movement
+        panMovement.y = 0;
 
         transform.Translate(panMovement, Space.World);
 
@@ -42,38 +42,32 @@ public class CameraController : MonoBehaviour
         }
     }
 
-void RotateCameraAroundViewportCenter(float targetRotation)
-{
-    // Create a layer mask for the ground plane
-    int groundLayerMask = 1 << LayerMask.NameToLayer("Ground");
-
-    // Raycast from the camera's viewport center to the ground plane
-    Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
-    RaycastHit hit;
-
-    // Check if the raycast hits the ground plane using the layer mask
-    if (Physics.Raycast(ray, out hit, Mathf.Infinity, groundLayerMask))
+    void RotateCameraAroundViewportCenter(float targetRotation)
     {
-        // Use the hit point on the ground plane as the pivot point
-        Vector3 pivotPoint = hit.point;
+        // Create a layer mask for the ground plane
+        int groundLayerMask = 1 << LayerMask.NameToLayer("Ground");
 
-        // Compute the relative position of the camera to the pivot point
-        Vector3 relativePosition = transform.position - pivotPoint;
+        // Raycast from the camera's viewport center to the ground plane
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
 
-        // Rotate the relative position by the target rotation
-        relativePosition = Quaternion.Euler(0, targetRotation, 0) * relativePosition;
+        // Check if the raycast hits the ground plane using the layer mask
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, groundLayerMask))
+        {
+            // Use the hit point on the ground plane as the pivot point
+            Vector3 pivotPoint = hit.point;
 
-        // Update the camera's position by adding the rotated relative position back to the pivot point
-        transform.position = pivotPoint + relativePosition;
+            // Compute the relative position of the camera to the pivot point
+            Vector3 relativePosition = transform.position - pivotPoint;
 
-        // Rotate the camera itself
-        transform.Rotate(Vector3.up, targetRotation, Space.World);
+            // Rotate the relative position by the target rotation
+            relativePosition = Quaternion.Euler(0, targetRotation, 0) * relativePosition;
+
+            // Update the camera's position by adding the rotated relative position back to the pivot point
+            transform.position = pivotPoint + relativePosition;
+
+            // Rotate the camera itself
+            transform.Rotate(Vector3.up, targetRotation, Space.World);
+        }
     }
-}
-
-
-
-
-
-
 }
